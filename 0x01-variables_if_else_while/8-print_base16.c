e69dcc7c006aecba5c55f28c87c892557a9ca322
Kamalznx <Zinixky@gmail.com>
2022-09-09 15:54:02 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int num = '0';
+	char num1 = 'a';
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (mum1 = 'a'; num1 <= 'f'; num1++)
+		putchar(num1);
+
+	putchar('\n');
+	return (0);
+}
