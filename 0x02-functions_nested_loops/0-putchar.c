eff648e96fa67d07ad6c13c6b94d95ad8f427027
Kamalznx <Zinixky@gmail.com>
2022-09-13 09:25:13 -0700

    initial task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - print putcher
+ * main - print putchar
  * Return: always 0
  */
 
 int main(void)
 {
-	_putcher('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
