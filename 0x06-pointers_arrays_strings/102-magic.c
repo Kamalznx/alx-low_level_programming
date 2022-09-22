0ace3857a6c85ff03f8041772331d75fe9bd7de8
Kamalznx <Zinixky@gmail.com>
2022-09-22 16:13:17 -0700

    Task 11

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,9 +13,10 @@ int main(void)
 	 * Remember:
 	 * - you are not allowed to use a variable in your new line
 	 *   of code
-	 * - you are not allowed to modift p
+	 * - you are not allowed to modify  p
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
+	 *
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
