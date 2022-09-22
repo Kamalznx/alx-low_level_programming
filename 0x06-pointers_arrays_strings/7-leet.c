7220607ac7b47a8964a2007b5e417445c0a947ab
Kamalznx <Zinixky@gmail.com>
2022-09-22 13:53:22 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - entry point
+ * ONE if, RWO loops only...
+ * @n: input
+ * Return: Always 0 (success)
+ */
+char *leet(char *n)
+{
+	int a, b;
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replace[] = ('4', '3', '0', '7', '1'};
+
+			for (i = 0; n[a] != 0; a++)
+			{
+			for (b = 0; b <= 9; b++)
+			{
+			if (n[a] == find[b])
+			{
+			n[a] = replace[b / 2];
+			b = 9;
+			}
+			}
+			}
+
+			return (n);
+}
